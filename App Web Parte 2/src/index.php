d481eaf41cfd7c07b1ec1b0a28660eedad66bf42
Paco Mañó <fj.manofrasquet@edu.gva.es>
2023-11-17 11:47:46 +0100

    Cambio Apps

--- App Web Parte 2/src/index.php
+++ App Web Parte 2/src/index.php
@@ -1,21 +1,49 @@
 <?php
-$debug= true;
-if($debug){
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-}
+// including the database connection file
+include_once("config.php");
 
-
-include "core/autoload.php";
-ob_start();
-session_start();
-
-// si quieres que se muestre las consultas SQL debes decomentar la siguiente linea
-// Core::$debug_sql = true;
-
-
-$lb = new Lb();
-$lb->start();
-
+// fetching data in descending order (lastest entry first)
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Homepage</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"  crossorigin="anonymous">	
+</head>
+
+<body>
+<div class = "container">
+	<div class="jumbotron">
+			<h1 class="display-4">Directorio Usuarios</h1>
+			<p class="lead">Examen 1ª Evaluación</p>
+			<p class="lead">Aquí debes indicar en qué servidor estás</p>
+    </div>	
+	<a href="add.html" class="btn btn-primary">Add New Data</a><br/><br/>
+	<table width='80%' border=0 class="table">
+
+	<tr bgcolor='#CCCCCC'>
+		<td>Nombre</td>
+		<td>Edad</td>
+		<td>Email</td>
+		<td>Update</td>
+	</tr>
+
+	<?php
+	while($res = mysqli_fetch_array($result)) {
+		echo "<tr>\n";
+		echo "<td>".$res['name']."</td>\n";
+		echo "<td>".$res['age']."</td>\n";
+		echo "<td>".$res['email']."</td>\n";
+		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>\n";
+		echo "</tr>\n";
+	}
+
+	mysqli_close($mysqli);
+	?>
+	</table>
+</div>
+</body>
+</html>
